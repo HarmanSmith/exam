f80b6953db79837d1f967df5d30a695fa41697c0
HarmanSmith <88251720+HarmanSmith@users.noreply.github.com>
2021-08-13 22:51:43 -0300

    singleton1

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public abstract class BasePage {
 
     protected WebDriver driver;
-    protected static WebDriverWait wait = null;
+    protected static WebDriverWait wait = null;//singleton
 
     //construct
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(this.getDriver(), this);
-        if(this.wait==null){
+        if(this.wait==null){ //singleton
             this.wait = new WebDriverWait(this.driver, 30);
         }
     }
